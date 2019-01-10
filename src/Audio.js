f5af19335c4d6cb2335f98e21d95f9a2d99dde18
kirbycampbell <jkirbycampbell@gmail.com>
2019-01-10 10:04:16 -0800

    Update Audio Page

--- src/Audio.js
+++ src/Audio.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import ReactPlayer from "react-player";
+import * as moment from "moment";
 
 class Audio extends Component {
   state = {};
+
+  tick() {
+    this.setState(time => moment().format("MMMM Do YYYY, h:mm:ss a"));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <div>
+        <DateMan />
+
         <div className="container pt-3">
           <div className="row justify-content-md-center">
             <ReactPlayer url="https://soundcloud.com/yessiejamez/drown-you-out-you-saved-me-ft-serpentine-man" />
@@ -37,3 +52,7 @@ class Audio extends Component {
 }
 // Maybe Cut these down to little samples of each track and make nice visual playlist
 export default Audio;
+
+const DateMan = () => {
+  return <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>;
+};
